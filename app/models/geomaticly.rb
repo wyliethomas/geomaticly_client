2ff8c0c15cc1e5a988acdf020706109f8aa85102
Wylie Thomas <wylie@wyliethomas.com>
2014-03-10 14:01:38 -0600

    initial commit

---
+++ app/models/geomaticly.rb
@@ -0,0 +1,39 @@
+require 'net/http'
+require 'json'
+require 'ostruct'
+
+class Geomaticly
+
+
+  def testobj
+    item1 = {:headline => "this is the headline", :phone => "111-111-1111"}
+    blob = OpenStruct.new(item1)
+    #obj = test.testobj.name
+    return blob
+  end
+
+  #creates a page object containing all the blocks for this page.
+  def self.page
+    response = apicall
+    blocks = {}
+    for item in response
+      if !item["title"].empty?
+        block = {item["title"] => item["body"]}
+        blocks.merge!(block)
+      end
+    end
+    blob = OpenStruct.new(blocks)
+    return blob
+  end
+
+
+  def self.apicall
+    #TODO: add geo and lang to URL params?
+    uri = URI.parse("http://127.0.0.1:3001/api/v1/pages/4")
+    http = Net::HTTP.new(uri.host, uri.port)
+    response = http.request(Net::HTTP::Get.new(uri.request_uri))
+    json = JSON.parse(response.body)
+    return json
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeomaticlyClient::Application.config.secret_token = '83419058b93ed6f3e753d68dfdefc7cfe8ee187010658026f30837999a3333e6660fa2fec39d2e6fb019996291e7f1b9f60cb626467c37f6d9fcd2214d8e7798'
