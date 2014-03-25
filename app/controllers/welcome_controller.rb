5c0d17c87b77dea6ea654ef7b31324b55f930409
Wylie Thomas <wylie@wyliethomas.com>
2014-03-25 17:28:07 -0600

    config settings for test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
+    p "IP"
+    p @remote_ip
     I18n.locale = 'en'
     @geoPage = Geomaticly.page('rvSkxbW3PdgWZwRQ')
   end
